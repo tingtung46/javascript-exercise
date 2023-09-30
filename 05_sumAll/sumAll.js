30657c250d17f9aa8119bbc4104000227f4b9eaa
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-09-30 22:40:06 +0700

    Add function to sum the number from parameter

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return "ERROR"; 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start > end) {
+        let reversal = start;
+        start = end;
+        end = reversal;
+    }
 
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
