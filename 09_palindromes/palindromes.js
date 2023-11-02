6a232baf800de762918ff4c47a06cb84a216da74
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-11-02 20:22:20 +0700

    Add palidromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const re = /[\W_]/g;
+    const lowRegStr = string.toLowerCase().replace(re, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
