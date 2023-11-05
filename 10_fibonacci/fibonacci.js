c7d4b7d51bf375877ec994773573afe4826c137e
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-11-05 17:54:39 +0700

    Add solution of fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let num = parseInt(number)
+    let num0 = 0;
+    let num1 = 1;
+    let result;
 
+    if (num < 0) return 'OOPS';
+    for (i = 1; i < num; i++) {
+        result = num0 + num1;
+        num0 = num1;
+        num1 = result;
+    }
+    return num1;
 };
 
 // Do not edit below this line
